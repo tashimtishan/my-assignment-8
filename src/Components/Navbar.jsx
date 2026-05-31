de23282ee692a5b4a308062e062667d963743df9
tashimtishan <tashimtishan01611412720@gmail.com>
2026-05-31 10:17:07 +0600

    first commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,29 @@
+import {GraduationCap} from '@gravity-ui/icons';
+import {ArrowRightToSquare} from '@gravity-ui/icons';
+import {PersonPlus} from '@gravity-ui/icons';
+import Link from 'next/link';
+const Navbar = () => {
+    return (
+       <nav className='bg-linear-to-r from-blue-900 via-blue-700 to-cyan-500'>
+      <div className='flex justify-between container mx-auto items-center p-5'>
+          <div className='flex items-center gap-2 text-white'>
+        <GraduationCap className='h-8 w-8'/>
+        <p className='font-bold text-xl'>SkillSphere</p>
+        </div>
+        <div>
+        <ul className='flex gap-5 text-white font-semibold'>
+            <Link href={""}><li>Home</li></Link>
+            <Link href={""}><li>Courses</li></Link>
+            <Link href={""}><li>My Profile</li></Link>
+        </ul>
+        </div>
+        <div className='flex gap-5 items-center'>
+        <button className='btn w-25 flex items-center'><ArrowRightToSquare></ArrowRightToSquare>Login</button>
+        <button className='btn btn-info w-30 text-white'><PersonPlus className='h-4 w-4'></PersonPlus>Register</button>
+        </div>
+      </div>
+       </nav>
+    );
+};
+
+export default Navbar;
