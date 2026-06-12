cbf99b17ad0bf0a9deb921547bbfe9a3798edfd8
tashimtishan <tashimtishan01611412720@gmail.com>
2026-06-12 16:39:30 +0600

    final changes

--- src/app/page.js
+++ src/app/page.js
@@ -6,17 +6,28 @@ import Teacherssection from "@/Components/Teacherssection";
 import TrendingCouse from "@/Components/TrendingCouse";
 import Whychooseus from "@/Components/Whychooseus";
 import Image from "next/image";
+import { Suspense } from 'react';
 
 export default function Home() {
   return (
-   <main className="bg-[#F7F7F7]">
-    <Banner></Banner>
-    <Highrated></Highrated>
-    <TrendingCouse></TrendingCouse>
-    <Learningtips></Learningtips>
-    <Teacherssection></Teacherssection>
-    <Whychooseus></Whychooseus>
-    <Learningjourney></Learningjourney>
-   </main>
+    <main className="bg-[#F7F7F7]">
+      <Banner></Banner>
+      <Suspense fallback={
+        <div className="flex justify-center items-center py-20">
+          <span className="loading loading-spinner loading-lg text-blue-600"></span>
+        </div>}>
+        <Highrated />
+      </Suspense>
+      <Suspense fallback={
+        <div className="flex justify-center items-center py-20">
+          <span className="loading loading-spinner loading-lg text-blue-600"></span>
+        </div>}>
+      <TrendingCouse />
+      </Suspense>
+      <Learningtips></Learningtips>
+      <Teacherssection></Teacherssection>
+      <Whychooseus></Whychooseus>
+      <Learningjourney></Learningjourney>
+    </main>
   );
 }
